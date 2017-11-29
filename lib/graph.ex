08767503d1952e30a968d0aa8a6ac77376e9461d
Langhaarzombie <mixmax199989@gmail.com>
2017-11-29 18:48:09 +0100

    Structure of Graph

--- lib/graph.ex
+++ lib/graph.ex
@@ -1,18 +1,26 @@
 defmodule Graph do
-  @moduledoc """
-  Documentation for Graph.
-  """
 
-  @doc """
-  Hello world.
+  defstruct nodes: %{}, edges: %{}
 
-  ## Examples
+  @type node_id :: atom
+  @type edge_id :: {node_id, node_id}
+  @type costs :: non_neg_integer
+  @type label :: term
+  @type t :: %__MODULE__{
+    nodes: %{node_id => %{label: label, costs: costs}},
+    edges: %{{node_id, node_id} => costs}
+  }
 
-      iex> Graph.hello
-      :world
+  # Nodes is defined as %{key(atom) => %{label => "string", hcosts => int}}
+  # Edges is defined as %{{from(atom), to(atom)} => costs(int)}
 
-  """
-  def hello do
-    :world
+  def add_edge(%__MODULE__{nodes: n, edges: e} = g, from, to, weight) when is_atom(from) and is_atom(to) do
   end
+
+  def add_node(%__MODULE__{nodes: n} = g, from, to, opts) when is_atom(from) and is_atom(to) and is_map(opts) do
+  end
+
+  def shortes_path(%__MODULE__{nodes: n, edges: e} = g, from, to) when is_atom(from) and is_atom(to) do
+  end
+
 end

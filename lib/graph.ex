0abb1bd3f78cdab811161bfb6b1430700a08da2a
Langhaarzombie <mixmax199989@gmail.com>
2017-11-29 19:42:53 +0100

    Adding and deleting nodes, adding edges

--- lib/graph.ex
+++ lib/graph.ex
@@ -11,13 +11,45 @@ defmodule Graph do
     edges: %{{node_id, node_id} => costs}
   }
 
+  # NOTE This module only serves as a library for undirected graphs!
   # Nodes is defined as %{key(atom) => %{label => "string", hcosts => int}}
   # Edges is defined as %{{from(atom), to(atom)} => costs(int)}
 
-  def add_edge(%__MODULE__{nodes: n, edges: e} = g, from, to, weight) when is_atom(from) and is_atom(to) do
+  def new do
+    %__MODULE__{}
   end
 
-  def add_node(%__MODULE__{nodes: n} = g, from, to, opts) when is_atom(from) and is_atom(to) and is_map(opts) do
+  def add_edge(%__MODULE__{nodes: n, edges: e} = g, from, to, costs) when is_atom(from) and is_atom(to) do
+    case Map.get(e, {from, to}) do
+      nil ->
+        g = g
+          |> add_node(from)
+          |> add_node(to)
+        %__MODULE__{g | edges: Map.put(e, {from, to}, costs)}
+    end
+  end
+
+  def add_node(%__MODULE__{nodes: n} = g, node) when is_atom(node) do
+    case Map.get(n, node) do
+      nil ->
+        add_node(g, node, %{label: nil, costs: nil})
+      _ ->
+        add_node(g, node, Map.get(n, node))
+    end
+  end
+
+  def add_node(%__MODULE__{nodes: n} = g, node, opts) when is_atom(node) and is_map(opts) do
+    case Map.get(n, node) do
+      nil ->
+        %__MODULE__{g | nodes: Map.put(n, node, %{label: Map.get(opts, :label), costs: Map.get(opts, :costs)})}
+      _ ->
+        g |> delete_node(node)
+          |> add_node(node, opts)
+    end
+  end
+
+  def delete_node(%__MODULE__{nodes: n} = g, node) when is_atom(node) do
+    %__MODULE__{g | nodes: Map.delete(n, node)}
   end
 
   def shortes_path(%__MODULE__{nodes: n, edges: e} = g, from, to) when is_atom(from) and is_atom(to) do

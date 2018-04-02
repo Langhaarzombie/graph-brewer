c4a65d5734edf8b3e268387a7ee5a5108dc9861f
Langhaarzombie <mixmax199989@gmail.com>
2018-04-02 17:36:29 +0200

    New version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Graph.Mixfile do
   def project do
     [
       app: :graphbrewer,
-      version: "0.1.5",
+      version: "0.1.6",
       elixir: "~> 1.6.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
